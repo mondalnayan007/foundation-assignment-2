a448fb134833cc2458c424e20d41dcc4f69aa809
Nayan Mondal <n007kingkong@gmail.com>
2026-09-16 23:40:02 +0600

    setting up the react router

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layout/RootLayout";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children:[
+        {
+            
+        }
+    ]
+  },
+]);
