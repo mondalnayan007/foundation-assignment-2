42823ae95e61b83d489c76fcf70be06fc1f30068
Nayan Mondal <n007kingkong@gmail.com>
2026-09-16 23:20:16 +0600

    started the project and set up the tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-2xl font-bold flex items-center justify-center min-h-screen'>Hello Movie Lovers</h1>
+    </>
+  )
+}
+
+export default App
